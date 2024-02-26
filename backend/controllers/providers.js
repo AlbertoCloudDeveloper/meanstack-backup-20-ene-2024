d08d6c40239e70a41280c327cedb7e160a0d4355
Albert Diaz <carlosalberto.diazjimenez@gmail.com>
2024-02-26 13:08:46 -0500

    Update 26Feb2024

--- backend/controllers/providers.js
+++ backend/controllers/providers.js
@@ -1,5 +1,23 @@
-const companys = require('../models/providers');
-
+const providers = require('../models/providers');
+// List
 module.exports.list = function(req,res){
-    res.render('providers/provider-list', { providers : companys})
+    res.render('providers/providers-list', {title:'Service Providers', providers : providers})
+}
+// Details
+module.exports.details = function(req,res){
+    let id = req.params.id;
+    let provider = providers.find( prodiver => prodiver.id == id)
+    res.render('providers/providers-details', { id : id,  title:'Service Providers Details', company : provider.company} )
+}
+// Edit Form
+module.exports.edit = function(req,res){
+    let id = req.params.id;
+    let provider = providers.find( prodiver => prodiver.id == id)
+    res.render('providers/providers-edit', { id : id,  title:'Edit', provider : provider} )
+}
+// Update Form
+module.exports.update = function(req,res){
+    let id = req.params.id;
+    let provider = providers.find( prodiver => prodiver.id == id)
+    res.render('providers/providers-update', { title:'Update', provider : provider} )
 }
--- backend/routes/providers.js
+++ backend/routes/providers.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const providerController = require('../controllers/providers')
+const providersController = require('../controllers/providers')
 
 /* GET list page. */
-router.get('/', providerController.list);
-
+router.get('/', providersController.list);
+/* GET details page. */
+router.get('/details/:id', providersController.details);
+/* GET edit page. */
+router.get('/edit/:id', providersController.edit);
+/* GET post page. */
+router.post('/update/:id', providersController.update);
 
 module.exports = router;
