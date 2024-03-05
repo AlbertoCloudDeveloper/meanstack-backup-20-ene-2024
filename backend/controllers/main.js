4d9d734fa2df260f2ee0cc3ad758a9701aad2765
Albert Diaz <carlosalberto.diazjimenez@gmail.com>
2024-03-05 08:27:07 -0500

    restore backup on dev2

--- backend/controllers/main.js
+++ backend/controllers/main.js
@@ -1,7 +1,7 @@
 // main controller
 
 module.exports.home = function(req, res){
-    res.render('index', { title: 'MEAN Stack Agency Dev2' });
+    res.render('index', { title: 'MEAN Stack Agency' });
 }
 
 module.exports.about = function(req, res){
